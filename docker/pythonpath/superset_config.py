c51685522056e663d07799b842f072e18a3a54bf
Rudo Kemper <rtakemper@gmail.com>
2026-07-09 20:25:22 -0400

    Use theme overrides in superset_config.py

--- docker/pythonpath/superset_config.py
+++ docker/pythonpath/superset_config.py
@@ -53,9 +53,25 @@ def get_env_variable(var_name: str, default: Optional[str] = None) -> str:
             raise EnvironmentError(error_msg)
 
 
-APP_NAME = get_env_variable("APP_NAME", "Superset")
+# Enable UI-based theme administration for admins
+# https://superset.apache.org/admin-docs/configuration/theming/
+ENABLE_UI_THEME_ADMINISTRATION = True
 
+APP_NAME = get_env_variable("APP_NAME", "Superset")
+BRAND_LOGO_URL = get_env_variable(
+    "BRAND_LOGO_URL", "/static/assets/images/superset-logo-horiz.png"
+)
+
+# For backward compatibility
 APP_ICON = get_env_variable("APP_ICON", "/static/assets/images/superset-logo-horiz.png")
+
+THEME_DEFAULT = {
+    "token": {"brandAppName": APP_NAME, "brandLogoUrl": BRAND_LOGO_URL or APP_ICON}
+}
+
+THEME_DARK = {
+    "token": {"brandAppName": APP_NAME, "brandLogoUrl": BRAND_LOGO_URL or APP_ICON}
+}
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
