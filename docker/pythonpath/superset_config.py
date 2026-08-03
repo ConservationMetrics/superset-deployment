35ede43a9a84b62db3e571f18f1139e1652f4d3c
Rudo Kemper <31662219+rudokemper@users.noreply.github.com>
2026-08-03 12:52:16 -0400

    [auth0] Fall back to email if first and last names not found (#65)
    * Fallback to empty string for first and last name
    
    * use optional 2nd argument in dict.get

--- docker/pythonpath/superset_config.py
+++ docker/pythonpath/superset_config.py
@@ -235,8 +235,10 @@ class CustomSecurityManager(SupersetSecurityManager):
             return {
                 "username": me["email"],
                 "email": me["email"],
-                "first_name": me["given_name"],
-                "last_name": me["family_name"],
+                "first_name": me.get(
+                    "given_name", ""
+                ),  # Auth0 will not return first (or last) name if users signed up with email and password instead of social login
+                "last_name": me.get("family_name", ""),
                 "role_keys": me.get("urn.gc.roles", []),
             }
 
